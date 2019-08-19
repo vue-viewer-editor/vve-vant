c6f32eb0b02a6b549167e929cd04e6a3fa0e6546
nianqin <nianqin.vip@gmail.com>
2019-08-19 19:54:03 +0800

    支持rem

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     autoprefixer: {},
     'postcss-pxtorem': {
-      rootValue: 16,
+      rootValue: 37.5,
       propList: ['*'],
       // 该项仅在使用 Circle 组件时需要
       // 原因参见 https://github.com/youzan/vant/issues/1948
